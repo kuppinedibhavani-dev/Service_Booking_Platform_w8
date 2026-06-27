884e7b3f7cec912e9114ad3faf84696b01834049
kuppinedibhavani_dev <kuppinedibhavani@gmail.com>
2026-06-27 20:34:29 +0530

    Cleared server.js file

--- backend/server.js
+++ backend/server.js
@@ -1,36 +1,36 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+
 const connectDB = require("./config/db");
+
 const authRoutes = require("./routes/authRoutes");
 const serviceRoutes = require("./routes/serviceRoutes");
 const bookingRoutes = require("./routes/bookingRoutes");
 const paymentRoutes = require("./routes/paymentRoutes");
 
-
 const app = express();
-connectDB();
-const cors = require("cors");
 
-app.use(
-  cors({
-    origin: [
-      "http://localhost:3000",
-      "https://service-booking-platform-w8.vercel.app"
-    ],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true
-  })
-);
+// Connect DB
+connectDB();
+
+// CORS
+app.use(cors());
+
+// Body parser
 app.use(express.json());
+
+// Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/services", serviceRoutes);
 app.use("/api/bookings", bookingRoutes);
 app.use("/api/payment", paymentRoutes);
 
+// Test route
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
